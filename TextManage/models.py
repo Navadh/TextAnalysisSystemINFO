0eaf7af12d3d03f549833532c25c788a0ef8be8a
sucui <15737239019@163.com>
2020-07-12 22:35:06 +0800

    06

--- TextManage/models.py
+++ TextManage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TextMange(models.Model):
+    ttextmanageid=models.AutoField(primary_key=True)
+    userid = models.IntegerField()
+    updatetime=models.DateTimeField()
+    ptype=models.CharField(max_length=45)
+    pname=models.CharField(max_length=45)
+
+
+    class Meta:
+        db_table="TTextManage"
+
