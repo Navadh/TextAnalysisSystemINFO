5ee6fc972a93c09d4294cc067ae5afb76ee6c13e
rajeshpahari <rajeshpahariemail@gmail.com>
2020-07-14 08:15:10 -0500

    Updating model file by Rajesh

--- TextAnalysis/models.py
+++ TextAnalysis/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from TextManage.models import TextMange
 
 class WFreq(models.Model):
-    ttextmanageid = models.ForeignKey(TextMange, on_delete=models.CASCADE)
     wordid = models.AutoField(primary_key=True)
+    ttextmanageid = models.ForeignKey(TextMange, on_delete=models.CASCADE)
+    #ttextmanageid = models.ForeignKey('TextManage.TTextManage', on_delete=models.CASCADE, db_column='ttextmanageid')
     word = models.CharField(max_length=45,null=False)
     frequency = models.CharField(max_length=5,null=False)
 
