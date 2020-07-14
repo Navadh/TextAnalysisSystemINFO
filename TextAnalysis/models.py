a8567300cc9f84cc5746e92ae48162fe0fd9995b
rajeshpahari <rajeshpahariemail@gmail.com>
2020-07-13 19:11:27 -0500

    Updating WFreq class

--- TextAnalysis/models.py
+++ TextAnalysis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from TextManage.models import TextMange
 
 class WFreq(models.Model):
-    pid = models.ForeignKey(TextMange, on_delete=models.CASCADE)
+    ttextmanageid = models.ForeignKey(TextMange, on_delete=models.CASCADE)
     wordid = models.AutoField(primary_key=True)
     word = models.CharField(max_length=45,null=False)
     frequency = models.CharField(max_length=5,null=False)
